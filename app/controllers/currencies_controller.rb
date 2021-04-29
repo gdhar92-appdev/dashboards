b0c81658017a79617d7e2ff9fcc06ff541b521c6
gdhar92 <gdhar@chicagobooth.edu>
2021-04-29 05:46:00 +0000

    Second Level Done

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -9,4 +9,15 @@ class CurrenciesController < ApplicationController
     render({ :template => "currency_templates/step_one.html.erb"})
   end
 
+  def second_currency
+    @currency = params.fetch("from_currency")
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+
+    @array_of_symbols = @symbols_hash.keys
+    render({ :template => "currency_templates/step_two.html.erb"})
+
+  end
+
 end
